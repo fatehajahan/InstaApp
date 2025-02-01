47bb872fa3623fc627879c64feedf7379571bb38
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-02-01 19:25:46 +0600

    done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,3 +1,4 @@
+import login from '../../assets/login.png'
 import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
 import React, { useState } from 'react'
 import { FaEye, FaEyeSlash, FaGoogle } from 'react-icons/fa'
@@ -48,9 +49,9 @@ const Login = () => {
                 .catch((error) => {
                     const errorCode = error.code;
                     const errorMessage = error.message;
-                   if(errorCode.includes('auth/invalid-credential')){
-                    toast.error('Please enter your registered email & password!')
-                   };
+                    if (errorCode.includes('auth/invalid-credential')) {
+                        toast.error('Please enter your registered email & password!')
+                    };
                 });
 
         }
@@ -74,65 +75,71 @@ const Login = () => {
             });
     }
     return (
-        <div className='bg-white shadow-2xl w-[700px] mx-auto px-[50px] py-[25px] rounded-xl mt-[60px]'>
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition={Bounce}
-            />
-            <div className="title">
-                <p className='font-Pacifico text-[60px] text-center'>InstaApp</p>
-            </div>
-
-            <div className="loginInfo">
-                <div className="email">
-                    <input onChange={handleEmail} type="email" placeholder='Please Enter your Email id' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
-                    <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{emailErr}</p>
+        <div className='flex  justify-between items-center p-[30px]'>
+            <div className=' ml-[90px] bg-white shadow-2xl w-[700px] px-[50px] py-[25px] rounded-xl mt-[60px]'>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                    transition={Bounce}
+                />
+                <div className="title">
+                    <p className='font-Pacifico text-[60px] text-center'>InstaApp</p>
                 </div>
 
-                <div onChange={handlePassword} className='password relative'>
-                    <input type={`${showPassword ? 'text' : 'password'
-                        }`} placeholder='Please Enter a Password' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
-                    {
-                        showPassword
-                            ? <FaEyeSlash onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
-                            : <FaEye onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
-                    }
+                <div className="loginInfo">
+                    <div className="email">
+                        <input onChange={handleEmail} type="email" placeholder='Please Enter your Email id' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
+                        <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{emailErr}</p>
+                    </div>
+
+                    <div onChange={handlePassword} className='password relative'>
+                        <input type={`${showPassword ? 'text' : 'password'
+                            }`} placeholder='Please Enter a Password' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
+                        {
+                            showPassword
+                                ? <FaEyeSlash onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
+                                : <FaEye onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
+                        }
 
 
-                    <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{passwordErr}</p>
+                        <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{passwordErr}</p>
+                    </div>
+                </div>
+
+                <div onClick={handleLogIn} className='bg-[#ff5acb] w-[300px] mx-auto text-center rounded-full py-[10px] mt-[15px] cursor-pointer'>
+                    <p className=' text-white font-roboto font-bold text-[18px]'>Log In</p>
+                </div>
+
+                <div className='bg-[#DBDBDB] w-full h-[3px] mt-[30px] relative'>
+                    <p className='absolute top-[-12px] left-[47%] bg-white px-[10px] font-roboto text-[18px] text-[#737373]'>OR</p>
+                </div>
+
+                <div onClick={handleGoogleLogin} className="loginWithGoogle flex items-center justify-center gap-x-[15px] bg-[#ff5acb] w-[300px] mx-auto py-[10px] mt-[30px] rounded-full cursor-pointer">
+                    <FaGoogle className='text-[25px] text-white' />
+                    <p className='text-[17px] text-white font-bold'>Log in with Google</p>
+                </div>
+
+                <div className='text-center pt-[10px]'>
+                    <Link to='/forgotpassword' className='text-[#737373] cursor-pointer text-center pt-[20px] text-[18px]'>Forgot Password?</Link>
+                </div>
+
+                <div className="alreadyHaveAcc bg-white border py-[20px] text-center mt-[20px] cursor-pointer">
+                    <Link to='/' >
+                        <p className='font-roboto'>Don't Have an Account? <span className='font-roboto text-[#ff5acb] font-bold'>Sign Up</span></p>
+                    </Link>
                 </div>
             </div>
 
-            <div onClick={handleLogIn} className='bg-[#ff5acb] w-[300px] mx-auto text-center rounded-full py-[10px] mt-[15px] cursor-pointer'>
-                <p className=' text-white font-roboto font-bold text-[18px]'>Log In</p>
-            </div>
-
-            <div className='bg-[#DBDBDB] w-full h-[3px] mt-[30px] relative'>
-                <p className='absolute top-[-12px] left-[47%] bg-white px-[10px] font-roboto text-[18px] text-[#737373]'>OR</p>
-            </div>
-
-            <div onClick={handleGoogleLogin} className="loginWithGoogle flex items-center justify-center gap-x-[15px] bg-[#ff5acb] w-[300px] mx-auto py-[10px] mt-[30px] rounded-full cursor-pointer">
-                <FaGoogle className='text-[25px] text-white' />
-                <p className='text-[17px] text-white font-bold'>Log in with Google</p>
-            </div>
-
-            <div className='text-center pt-[10px]'>
-                <Link to='/forgotpassword' className='text-[#737373] cursor-pointer text-center pt-[20px] text-[18px]'>Forgot Password?</Link>
-            </div>
-
-            <div className="alreadyHaveAcc bg-white border py-[20px] text-center mt-[20px] cursor-pointer">
-                <Link to='/' >
-                    <p className='font-roboto'>Don't Have an Account? <span className='font-roboto text-[#ff5acb] font-bold'>Sign Up</span></p>
-                </Link>
+            <div>
+                <img src={login} alt="" className='mx-auto mr-[90px]' />
             </div>
         </div>
     )
--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import image from '../../assets/signup.png'
 import { FaEye, FaEyeSlash, FaGoogle } from 'react-icons/fa'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
@@ -100,7 +101,7 @@ const Registration = () => {
       });
   }
   return (
-    <div className='bg-white shadow-2xl w-[700px] mx-auto px-[50px] py-[25px] rounded-xl mt-[60px]'>
+    <div className=' flex  justify-between items-center p-[30px]'>
       <ToastContainer
         position="top-center"
         autoClose={5000}
@@ -114,55 +115,61 @@ const Registration = () => {
         theme="light"
         transition={Bounce}
       />
-      <div className="title">
-        <p className='font-Pacifico text-[60px] text-center'>InstaApp</p>
-        <p className='font-roboto text-[16px] text-center w-[280px] mx-auto pt-[20px]'>Sign up to see photos and videos from your friends.</p>
-      </div>
-      <div onClick={googleSignUp} className="SignUpWithGoogle flex items-center justify-center gap-x-[15px] bg-[#ff5acb] w-[300px] mx-auto py-[10px] mt-[20px] rounded-full cursor-pointer">
-        <FaGoogle className='text-[25px] text-white' />
-        <p className='text-[17px] text-white font-bold'>Sign Up with Google</p>
-      </div>
-      <div className='bg-[#DBDBDB] w-full h-[3px] mt-[25px] relative'>
-        <p className='absolute top-[-12px] left-[47%] bg-white px-[10px] font-roboto text-[18px] text-[#737373]'>OR</p>
-      </div>
+      <div className='ml-[90px] bg-white shadow-2xl w-[700px] px-[50px] py-[25px] rounded-xl mt-[60px]'>
+        <div className="title">
+          <p className='font-Pacifico text-[60px] text-center'>InstaApp</p>
+          <p className='font-roboto text-[16px] text-center w-[280px] mx-auto pt-[20px]'>Sign up to see photos and videos from your friends.</p>
+        </div>
+        <div onClick={googleSignUp} className="SignUpWithGoogle flex items-center justify-center gap-x-[15px] bg-[#ff5acb] w-[300px] mx-auto py-[10px] mt-[20px] rounded-full cursor-pointer">
+          <FaGoogle className='text-[25px] text-white' />
+          <p className='text-[17px] text-white font-bold'>Sign Up with Google</p>
+        </div>
+        <div className='bg-[#DBDBDB] w-full h-[3px] mt-[25px] relative'>
+          <p className='absolute top-[-12px] left-[47%] bg-white px-[10px] font-roboto text-[18px] text-[#737373]'>OR</p>
+        </div>
 
-      <div className="informations">
-        <div className="email">
+        <div className="informations">
           <div className="email">
-            <input onChange={handleEmail} type="email" placeholder='Please Enter your Email id' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
-            <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{emailErr}</p>
+            <div className="email">
+              <input onChange={handleEmail} type="email" placeholder='Please Enter your Email id' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
+              <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{emailErr}</p>
+            </div>
+
+            <div className="name">
+              <input onChange={handleName} type="text" placeholder='Please Enter Your Name' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
+              <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{nameErr}</p>
+            </div>
+
+            <div onChange={handlePassword} className='password relative'>
+              <input type={`${showPassword ? 'text' : 'password'
+                }`} placeholder='Please Enter a Password' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
+              {
+                showPassword
+                  ? <FaEyeSlash onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
+                  : <FaEye onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
+              }
+
+
+              <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{passwordErr}</p>
+            </div>
+
+            <p className='text-center w-[450px] mx-auto font-roboto pt-[23px] text-[#8587B7]'>By signing up, you agree to our <span className='text-[#00376B]'>Terms , Privacy Policy</span> and <span className='text-[#00376B]'>Cookies Policy .</span></p>
           </div>
+        </div>
 
-          <div className="name">
-            <input onChange={handleName} type="text" placeholder='Please Enter Your Name' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
-            <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{nameErr}</p>
-          </div>
+        <div onClick={handleSubmit} className='bg-[#ff5acb] w-[300px] mx-auto text-center rounded-full py-[10px] mt-[15px] cursor-pointer'>
+          <p className=' text-white font-roboto font-bold text-[18px]'>Sign up</p>
+        </div>
 
-          <div onChange={handlePassword} className='password relative'>
-            <input type={`${showPassword ? 'text' : 'password'
-              }`} placeholder='Please Enter a Password' className='px-[8px] py-[10px] w-full mt-[25px] border bg-[#FAFAFA] rounded-md' />
-            {
-              showPassword
-                ? <FaEyeSlash onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
-                : <FaEye onClick={() => setShowPassword(!showPassword)} className='absolute top-[55%] right-[4%] cursor-pointer' />
-            }
-
-
-            <p className='text-red-600 font-roboto font-bold text-[12px] absolute'>{passwordErr}</p>
-          </div>
-
-          <p className='text-center w-[450px] mx-auto font-roboto pt-[23px] text-[#8587B7]'>By signing up, you agree to our <span className='text-[#00376B]'>Terms , Privacy Policy</span> and <span className='text-[#00376B]'>Cookies Policy .</span></p>
+        <div className="alreadyHaveAcc bg-white border py-[20px] text-center mt-[20px] cursor-pointer">
+          <Link to='/login' >
+            <p className='font-roboto'>Have an Account? <span className='font-roboto text-[#ff5acb] font-bold'>Log in</span></p>
+          </Link>
         </div>
       </div>
 
-      <div onClick={handleSubmit} className='bg-[#ff5acb] w-[300px] mx-auto text-center rounded-full py-[10px] mt-[15px] cursor-pointer'>
-        <p className=' text-white font-roboto font-bold text-[18px]'>Sign up</p>
-      </div>
-
-      <div className="alreadyHaveAcc bg-white border py-[20px] text-center mt-[20px] cursor-pointer">
-        <Link to='/login' >
-          <p className='font-roboto'>Have an Account? <span className='font-roboto text-[#ff5acb] font-bold'>Log in</span></p>
-        </Link>
+      <div>
+        <img src={image} alt="" className='mr-[90px]' />
       </div>
     </div>
   )
